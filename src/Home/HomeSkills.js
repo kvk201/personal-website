e307abd28129d60529eb456c15442daf1e729a84
messi618 <54788160+messi618@users.noreply.github.com>
2023-03-28 16:49:05 -0500

    updated links

--- src/Home/HomeSkills.js
+++ src/Home/HomeSkills.js
@@ -3,7 +3,7 @@ function HomeSkills() {
       <div className="home-skills"> 
       <h2># SITE UNDER CONSTRUCTION </h2>
       <h2># view source code here: </h2>
-      <h3><a href='<https://github.com/messi618/VivekMessiKumar.com/a><'>https://github.com/messi618/VivekMessiKumar.com</a></h3>
+      <h3><a href='https://github.com/messi618/VivekMessiKumar.com'>Github Repository.</a></h3>
 
       </div>
     );
